41c40416a64f95b8905ba12561d8f5c45341b0c4
Niels ten Boom <niels.ten.boom@rtl.nl>
2020-01-23 07:57:44 +0100

    add comments and timedelta

--- src/app.py
+++ src/app.py
@@ -19,17 +19,40 @@ trips_parsed = []
 
 
 def get_station_uic(station_name):
+    """
+    Given a station name, return its uic, by searching in the stations list
+    
+    Args:
+        station_name (string): Exact name of the station
+    
+    Returns:
+        string: uic of the station
+    """
     for station in stations:
         if station_name == station.names['lang']:
             return station.uic_code
 
 
 def get_ns_trip(uic_start, uic_end, time_start, time_end):
+    """
+    Gets ns trip specification object given a route string
+    """
     result = ns.get_trip(f"arnu|fromStation={uic_start}|toStation={uic_end}|plannedFromTime={time_start}|plannedArrivalTime={time_end}|yearCard=false|excludeHighSpeedTrains=false")
     return result
 
 
-def parse_upcoming_trips(trips):
+def parse_upcoming_trips(trips, minutes_delta):
+    """
+    Loops through the specified trips and if the trip is within the next minutes_delta minutes
+    then the trip metadata is requested from the ns api and added to the result list
+    
+    Args:
+        trips (list[dict]): list of the specified trips
+    
+    Returns:
+        list: list of ns-objects of trips that are within next delta minutes
+    """
+
     result = []
     for trip in trips:
         station_start_uic = get_station_uic(trip["station_origin"])
@@ -43,13 +66,17 @@ def parse_upcoming_trips(trips):
         
         start = dt_start.strftime("%Y-%m-%dT%H:%M:%S+01:00")
         arrive = dt_arrive.strftime("%Y-%m-%dT%H:%M:%S+01:00")
-        if abs(diff_start) < 20 or abs(diff_end) < 20:
+
+        if abs(diff_start) < minutes_delta or abs(diff_end) < minutes_delta:
             trip_parsed = get_ns_trip(station_start_uic, station_end_uic, start, arrive)
             result.append(trip_parsed)
 
     return result
 
 def has_delays(trip):
+    """
+    Checks ns-trip object and looks if there is a delay in departure and arrival times
+    """
     t = trip["legs"][0]
 
     result = True
@@ -59,9 +86,10 @@ def has_delays(trip):
     
     return not result
 
-
+# list of trips defined in trips.csv
 trips = pd.read_csv("trips.csv").to_dict('records')
-trips_parsed = parse_upcoming_trips(trips)
+# list of the same trips but these are ns-objects
+trips_parsed = parse_upcoming_trips(trips, 30)
 
 for trip_parsed, trip in zip(trips_parsed, trips):
     if has_delays(trip_parsed):
